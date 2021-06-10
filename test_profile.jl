218be73665c103cff80dbc40becb0d52270ec27a
newton-climate <newt.n94@gmail.com>
2021-06-10 08:21:48 -0700

    Calculated averaging kernals and other information metrix

--- test_profile.jl
+++ test_profile.jl
@@ -7,10 +7,10 @@ include("profile.jl")
 using Plots
 
 
-num_layers = 30
+num_layers = 20
 # Just defining the spectral windows for each species
 ν_CH4 = (6055, 6120)
-                        ν_CO2 = (6205, 6260);
+                        ν_CO2 = (6180, 6260);
 ν_HDO = (6310,6380);
 
 ν_min, ν_max = ν_CO2[1], ν_CO2[2]
@@ -46,7 +46,7 @@ a = ones(size(p))
 
 # define the reetrieval parameters
 inversion_setup = Dict{String,Any}(
-    "poly_degree" => 3,
+    "poly_degree" => 10,
     "fit_pressure" => true,
     "fit_temperature" => true,
     "use_OCO" => true,
@@ -55,11 +55,11 @@ inversion_setup = Dict{String,Any}(
 
 # define the initial guess 
 x_true = OrderedDict{Any,Any}(H₂O => collect(range(0.01, 0.03, length=num_layers)),
-                              CO₂ => 1e-6 * collect(range(390, 405, length=num_layers)),
+                              CO₂ => 1e-6 * collect(range(395, 400, length=num_layers)),
                               CH₄ => 1e-9*collect(range(1800, 2000, length=num_layers)),
                   "pressure" => p,
                   "temperature" => T,
-                          "shape_parameters" => [maximum(measurement.intensity); zeros(inversion_setup["poly_degree"]-1)])
+                              "shape_parameters" => [maximum(measurement.intensity); zeros(inversion_setup["poly_degree"]-1)])
 
 
 # Make some synthetic data 
@@ -73,15 +73,16 @@ measurement.intensity = τ #+ ϵ
 
 
 xₐ = OrderedDict{Any,Any}(H₂O => 0.02*a,
-                          CO₂ => 397e-6*a,
-                          CH₄ => 1900e-9*a,
-                  "shape_parameters" => [maximum(measurement.intensity); zeros(inversion_setup["poly_degree"]-1)])
+                          CO₂ => 395e-6*a,
+                          CH₄ => 1800e-9*a,
+                          "shape_parameters" => [maximum(measurement.intensity); zeros(inversion_setup["poly_degree"]-1)])
 inversion_setup["fit_pressure"] = false
 
 σ = OrderedDict{Any,Any}(H₂O => 0.01*a,
                          CO₂ => 4.0e-6*a,
-                         CH₄ => 100e-9*a,
-                  "shape_parameters" => ones(inversion_setup["poly_degree"])) 
+                         CH₄ => 200e-9*a,
+                         "shape_parameters" => ones(inversion_setup["poly_degree"]))
+#1e-10*ones(inversion_setup["poly_degree"])) 
 inversion_setup = push!(inversion_setup, "σ" => σ)
 
 
@@ -98,7 +99,8 @@ x_retrieved = assemble_state_vector!(out.x, collect(keys(xₐ)), num_layers, inv
 
 x_retrieved[CO₂] = x_retrieved[CO₂] ./ (1 .- x_retrieved[H₂O])
 co2_con = 1e6*x_retrieved[CO₂]
-
+@show co2_con
+h2o_con = 1e2*x_retrieved[H₂O]
 ### Plot our results
 p1 = plot(measurement.grid, measurement.intensity, label="observed", color="black")
 plot!(measurement.grid, out.y, label="modelled", color="red")
@@ -135,7 +137,7 @@ x_retrieved = assemble_state_vector!(out2.x, collect(keys(xₐ)), num_layers, in
 
 
 x_retrieved[CH₄] = x_retrieved[CH₄] ./ (1 .- x_retrieved[H₂O])
-h2o_con = 1e2*x_retrieved[H₂O]
+
 ch4_con = x_retrieved[CH₄]
 @show 1e9*x_retrieved[CH₄]
 
@@ -172,7 +174,7 @@ h2o_ind = 1:num_layers
 co2_ind = num_layers+1:2*num_layers
 ch4_ind = 2*num_layers+1:3*num_layers
 
-H = vcd / sum(vcd)
+H = vcd ./ sum(vcd)
 cK_h2o = (H'*A2[h2o_ind, h2o_ind])'
 cK_co2 = (H'*A1[co2_ind, co2_ind])'
 cK_ch4 = (H'*A2[ch4_ind, ch4_ind])'
