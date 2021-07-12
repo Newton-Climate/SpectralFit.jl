6475b6cc381b92d36118ae1b422feb033591c8d5
newton-climate <newt.n94@gmail.com>
2021-07-12 15:04:10 -0700

    Added gravity constant

--- src/constants.jl
+++ src/constants.jl
@@ -2,6 +2,7 @@
 const c = 299792458 * 100 # speed of light cm/s
 const r = 8.314472; # universal gas constant joules / moles / K
 const Nₐ = 6.0221415e23; # molecules / moles
+const g₀ = 9.8196 #gravity
 
 # Define constants for retrieval
 global const H₂O_ind, CH₄_ind, CO₂_ind, HDO_ind = 1, 2, 3, 4;
