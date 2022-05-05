0a98f2270d5dd6d7617dccc54338ea918915eacd
newton-climate <newt.n94@gmail.com>
2022-05-05 17:14:33 -0500

    Added function to convert from dataset to Array{measurement} and fied time-averaging

--- src/read_data.jl
+++ src/read_data.jl
@@ -1,7 +1,10 @@
-using RadiativeTransfer, RadiativeTransfer.Absorption
-using Statistics , Interpolations, Dates
+include("constants.jl")
+include("types.jl")
+include("utils.jl")
+
+    using Statistics , Interpolations, Dates
 using OrderedCollections, HDF5, JLD2
-    
+
 
 
 
@@ -34,10 +37,10 @@ end
 
     pressure = 0.9938*HDF5.read(file, datafields[1]) # pressure in mbar
     temperature = HDF5.read(file, datafields[2]) + 273.15*ones(length(pressure)) # temperature in K
-    pathlength = 195017 # round trip path length in meters DCSA
+    pathlength = 195017.0 # round trip path length in meters DCSA
     #pathlength = 196367 # round trip path length in m for DCSB
 
-    time = HDF5.read(file, datafields[3])
+    machine_time = HDF5.read(file, datafields[3])
     grid = HDF5.read(file, datafields[4])
     grid = grid ./ c # convert from hz to wavenumber 
     intensity = HDF5.read(file, datafields[5])
@@ -45,64 +48,86 @@ end
 
     # calculate noise
     σ² = calc_DCS_noise(grid, intensity)
-    num_averaged_measurements, averaging_window = 1, nothing
+    averaging_window = Second(1)
+    n = length(pressure)
+    num_averaged_measurements = ones(Int64, n)
+    vcd = calc_vcd.(pressure, temperature, pathlength)
+    time = unix2datetime.(machine_time)
 
+
+    dataset = FrequencyCombDataset(filename=filename, intensity=intensity, grid=grid,
+                                              temperature=temperature, pressure=pressure,
+                                time=time, pathlength=pathlength,                                              num_averaged_measurements=num_averaged_measurements, averaging_window=averaging_window, machine_time=machine_time, σ²=σ², vcd=vcd)
+    
     # save to a struct
-    dataset = map(i->FrequencyCombMeasurement(filename=filename, intensity=intensity[i,:], grid, temperature=temperature[i], pressure=pressure[i], time=time[i], pathlength=pathlength,                                              num_averaged_measurements=num_averaged_measurements[i], averaging_window=averaging_window[i], machine_time=time[i], σ²=σ²[i])
+    #dataset = map(i->FrequencyCombMeasurement(intensity=intensity[i,:], grid=grid,
+#                                              temperature=temperature[i], pressure=pressure[i],
+#                                              time=time[i], #pathlength=pathlength,                                              #num_averaged_measurements=num_averaged_measurements, averaging_window=averaging_window, #machine_time=machine_time[i], σ²=σ²[i], vcd=vcd[i]), 1:n)
     return dataset
 end # function read_DCS_data
 
 
-function take_time_average(dataset::FrequencyCombDataset; δt::Period=Dates.Hour(1))
+
+
+
+function take_time_average!(dataset::FrequencyCombDataset; δt::Period=Dates.Hour(1))
     """
 -Takes time-average of the FrequencyCombDataset over a time δt::TimeDelta
 - returns a TimeAveragedFrequencyCombDataset 
 """
-    
-    timestamps = unix2datetime.(dataset.time)
+
+    FT = eltype(dataset.intensity)
+    IT = eltype(dataset.num_averaged_measurements)
+    timestamps = dataset.time
     t₁ = floor(timestamps[1], δt)
     t₂ = t₁ + δt
     t_final = timestamps[end]
     num_measurements = ceil((t_final - t₁), δt)
     num_measurements = Int(num_measurements/δt)
-    averaged_measurements = Array{Float64}(undef,(num_measurements, size(dataset.intensity)[2]))
-    averaged_temperature = Array{Float64}(undef, num_measurements)
-    averaged_pressure = Array{Float64}(undef, num_measurements)
-    averaging_times = Array{Tuple{DateTime,DateTime}}(undef, num_measurements)
-    averaged_σ² = Array{Float64,1}(undef, num_measurements)
-    num_averaged_measurements = Array{Int64}(undef, num_measurements)
-    machine_time = Array{Float64}(undef,num_measurements)
+
+    # allocate memory 
+    averaged_measurements = Array{FT}(undef,(num_measurements, size(dataset.intensity)[2]))
+    averaged_temperature = Array{FT}(undef, num_measurements)
+    averaged_pressure = Array{FT}(undef, num_measurements)
+    averaging_times = Array{DateTime}(undef, num_measurements)
+    averaged_σ² = Array{FT,1}(undef, num_measurements)
+    num_averaged_measurements = Array{IT}(undef, num_measurements)
+    machine_time = Array{FT}(undef,num_measurements)
+    averaged_vcd = similar(averaged_pressure)
     i= 1;
 
     while t₁ < t_final
         indexes = findall(t->(t>=t₁ && t<=t₂), timestamps)
         
         # in the case where we get no idnexes, this averaging won't fail
-        if length(indexes) < 1; break; end;
+        length(indexes)<1 ? n=length(indexes) : break
 
         averaged_measurements[i,:] = mean(dataset.intensity[indexes, :], dims=1)
         averaged_temperature[i] = mean(dataset.temperature[indexes])
         averaged_pressure[i] = mean(dataset.pressure[indexes])
-        averaged_σ²[i] = 1/length(indexes) * mean(dataset.σ²[indexes])
-        num_averaged_measurements[i] = length(indexes) # save number of averaged measurements per window
-        averaging_times[i] = (t₁, t₂)
+        averaged_σ²[i] = 1/n * mean(dataset.σ²[indexes])
+        num_averaged_measurements[i] = n
+        averaging_times[i] = t₁
         machine_time[i] = dataset.time[indexes[1]]
+        averaged_vcd[i] = mean(dataset.vcd[indexes])
         
-
         # update variables
         i += 1
         t₁ = t₂
         t₂ = t₂ + δt
     end # while loop
 
-    data_out = TimeAveragedFrequencyCombDataset(dataset.filename, averaged_measurements, dataset.grid, averaged_temperature, averaged_pressure, averaging_times, dataset.pathlength, num_averaged_measurements, δt, machine_time, averaged_σ²)
-    return data_out
+    dataset.intensity= averaged_measurements
+                                    dataset.temperature=averaged_temperature
+    dataset.pressure=averaged_pressure
+    dataset.time=averaging_times
+    dataset.num_averaged_measurements=num_averaged_measurements
+    dataset.averaging_window=δt
+    dataset.machine_time=machine_time
+    dataset.σ²=averaged_σ²
+    dataset.vcd=averaged_vcd
+    return dataset
 end
-
-    
-
-
-
 
 
 
@@ -111,33 +136,40 @@ end
 Subsets the FrequencyCombDataset into indivitual measurements 
 Constructor of Measurement type
 """
-function get_measurement(measurement_num::Integer, dataset::Dataset, ν_min::Real, ν_max::Real)
+function get_measurement(measurement_num::Integer, dataset::AbstractDataset, ν_min::Real, ν_max::Real)
 
     i = measurement_num
     p = dataset.pressure[i]
     T = dataset.temperature[i]
     δz = dataset.pathlength
     time = dataset.time[i]
-    σ² = dataset.σ²[i]
 
     # find indexes
     indexes = find_indexes(ν_min, ν_max, dataset.grid)
     grid = dataset.grid[indexes]
     intensity = dataset.intensity[i,indexes]
-    vcd = calc_vcd(p, T, δz)
 
     # save to struct FrequencyCombMeasurement
-    if typeof(dataset) == FrequencyCombDataset
-        measurement = FrequencyCombMeasurement(intensity, grid, T, p, time, δz, vcd, 1, 0, dataset.timestamp[i], σ)
-        elseif typeof(dataset) == TimeAveragedFrequencyCombDataset
-        measurement = FrequencyCombMeasurement(intensity, grid, T, p, time, δz, vcd, dataset.num_averaged_measurements[i], dataset.averaging_window, dataset.timestamp[i], σ²)
-    end # if statement
+    measurement = FrequencyCombMeasurement(intensity=intensity, grid=grid,
+                                           temperature=T, pressure=p, time=time,
+                                           pathlength=δz, vcd=dataset.vcd[i], num_averaged_measurements=dataset.num_averaged_measurements[i],
+                                           averaging_window=dataset.averaging_window, machine_time=dataset.machine_time[i], σ²=dataset.σ²[i])
     
     return measurement
 end # function get_measurement
 
+function data2measurements(dataset::AbstractDataset, ν_min=nothing, ν_max=nothing)
+    min = ν_min==nothing ? dataset.grid[1] : ν_min
+    max = ν_max==nothing ? dataset.grid[end] : ν_max
+    
+    n = length(dataset.pressure)
+    out = map(i->get_measurement(i, dataset, min, max), 1:n)
+    return out
+end
 
-unction get_measurement(measurement_num::Integer, dataset::Dataset, ν_min::Real, ν_max::Real, p::Array{Float64,1}, T::Array{Float64,1})
+    
+
+function get_measurement(measurement_num::Integer, dataset::AbstractDataset, ν_min::Real, ν_max::Real, p::Array{Float64,1}, T::Array{Float64,1})
 
     i = measurement_num
     δz = dataset.pathlength
@@ -150,14 +182,14 @@ unction get_measurement(measurement_num::Integer, dataset::Dataset, ν_min::Real
     vcd = make_vcd_profile(p, T)
 
     # save to struct FrequencyCombMeasurement
-    if typeof(dataset) == FrequencyCombDataset
-        measurement = FrequencyCombMeasurement(intensity, grid, T, p, time, δz, vcd, 1, 0, dataset.timestamp[i], dataset.σ[i])
-        elseif typeof(dataset) == TimeAveragedFrequencyCombDataset
-        measurement = FrequencyCombMeasurement(intensity, grid, T, p, time, δz, vcd, dataset.num_averaged_measurements[i], dataset.averaging_window, dataset.timestamp[i], dataset.σ[i])
-    end # if statement
-    
+        measurement = FrequencyCombMeasurement(intensity=intensity, grid=grid,
+                                           temperature=T, pressure=p, time=time,
+                                           pathlength=δz, vcd=vcd, num_averaged_measurements=dataset.num_averaged_measurements[i],
+                                           averaging_window=dataset.averaging_window, machine_time=dataset.machine_time[i], σ²=dataset.σ²[i])
     return measurement
 end # function get_measurement
+
+
 
 
     
@@ -197,5 +229,3 @@ Constructs an interpolation of the OCO cross-sections grid provided by JPL
     sitp = scale(itp, ν, T, p);
     return sitp
     end
-
-
