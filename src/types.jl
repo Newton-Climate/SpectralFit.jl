9d345e626c6d7f76609f0548c91239c3e0780e53
newton-climate <newt.n94@gmail.com>
2022-05-18 21:46:52 -0500

    Updated InversionResults to be keyword arguements

--- src/types.jl
+++ src/types.jl
@@ -3,20 +3,20 @@ using vSmartMOM.Absorption, Dates
 stores the metadata of hitran line-lists and coefficients in type HitranTable
 
 """
-Base.@kwdef struct MolecularMetaData{FT}
+Base.@kwdef struct MolecularMetaData
     molecule::String
     filename::String
     molecule_num::Int
     isotope_num::Int
-    ν_grid::Union{AbstractRange{Real}, Array{FT,1}}
+    grid::Union{AbstractRange, AbstractArray}
     model::AbstractCrossSectionModel
 end
 
 
 """type to store cross-sections, pressure, temperature, and line-list parameters"""
 Base.@kwdef mutable struct Molecule{FT}
-    cross_sections::Array{Real,1}
-    grid::Union{AbstractRange{FT}, Array{FT,1}}
+    cross_sections::Array{<:Real,1}
+    grid::Union{AbstractRange, Array{FT,1}}
     p::FT
     T::FT
     model::AbstractCrossSectionModel
@@ -29,39 +29,40 @@ end
 Base.@kwdef mutable struct InversionResults{FT}
     timestamp::DateTime
     machine_time::FT
-    x::Array{FT,1}
+    x::AbstractDict{String,Union{FT, Vector{FT}}}
     measurement::Array{FT,1}
     model::Array{FT,1}
     χ²::FT
     S::Array{FT,2}
     grid::Array{FT,1}
     K::Array{FT,2}
-    Sₑ::AbstractMatrix{FT}
-    Sₐ::AbstractMatrix{FT}
+    Sₑ::AbstractArray{FT}
+    Sₐ::AbstractArray{FT}
 end
 
 
 abstract type AbstractDataset end
 
 
-struct FrequencyCombDataset{FT} <: AbstractDataset
+Base.@kwdef mutable struct FrequencyCombDataset{FT} <: AbstractDataset
     filename::String
     intensity::Array{FT,2}
     grid::Vector{FT}
     temperature::Vector{FT}
     pressure::Vector{FT}
-    time::Vector{Tuple{DateTime,DateTime}}
+    time::Vector{DateTime}
     pathlength::FT
     num_averaged_measurements::Vector{Int}
     averaging_window::Dates.Period
-    timestamp::Vector{FT}
+    machine_time::Vector{FT}
     σ²::Vector{FT}
+    vcd::Vector{FT}
 end
 
 
 abstract type AbstractMeasurement end
 
-mutable struct FrequencyCombMeasurement{FT} <: AbstractMeasurement
+Base.@kwdef mutable struct FrequencyCombMeasurement{FT} <: AbstractMeasurement
     intensity::Vector{FT}
     grid::Vector{FT}
     temperature::Union{FT, Vector{FT}}
